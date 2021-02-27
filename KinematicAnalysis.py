ef8a1097fe407492dea8933d0a4869055519f2f0
SnakeofStone <jorgeruben99@gmail.com>
2021-02-27 01:17:41 -0600

    Complete a full kinematic analysis
    - Modify how the Zn and On dimensions are declared
    - Add FileNotFoundError exception handling
    - Add linearJacobian and angularJacobian creation function
    - Add angular and linear velocity calculation function
    - Print values in a format
    - Add cli support for different input files
    
    Signed-off-by: SnakeofStone <jorgeruben99@gmail.com>

--- KinematicAnalysis.py
+++ KinematicAnalysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import json
 import numpy as np
+import fire
 
 # Constant parameters
 THETA = 0
@@ -18,8 +19,11 @@ def readDataFromFile(file):
     try:
         with open(file, 'r') as dataFile:
             data = json.load(dataFile)
-    except json.decoder.JSONDecodeError:
+    except json.decoder.JSONDecodeError as err:
         print("Error: Invalid JSON file format")
+        exit(-1)
+    except FileNotFoundError as fnf:
+        print("Error: {}".format(fnf.strerror))
         exit(-1)
 
     return data
@@ -30,20 +34,17 @@ Create array of matrices from DH
 """
 def DH_Matrix(DH_Params, numDeg):
     try:
-        dh = np.array(DH_Params).reshape(numDeg, 4)
+        dh = np.array(DH_Params, dtype=np.single).reshape(numDeg, 4)
     except:
         print("Error while converting to matrix")
         exit(-1)
-
-    # Define matrix as single point precision
-    dh = dh.astype(np.single)
 
     # Convert from deg to rad
     dh = degToRad(dh)
 
     MT_n = np.zeros([numDeg, 4, 4]).astype(np.single)
     for vec in range(numDeg):
-        MT_n[vec] = dh_single(dh[vec])
+        MT_n[vec] = dh_single(dh[vec].transpose())
 
     return MT_n
 
@@ -51,27 +52,24 @@ def DH_Matrix(DH_Params, numDeg):
 Return the matrix of Zn
 """
 def getZn(numDeg, MT_n):
-    Zn = np.zeros((3, numDeg), np.single)
+    Zn = np.zeros((numDeg, 3), np.single)
 
-    Zn[2][0] = 1
+    Zn[0][2] = 1
     for col in range(numDeg - 1):
-        for line in range(3):
-            Zn[line][col+1] = MT_n[col][line][2]
+        Zn[col+1] = MT_n[col, 0:3, 2]
 
-    return Zn
+    return np.around(Zn, decimals=3)
 
 """
 Return the matrix On
 """
 def getOn(numDeg, MT, MT_n):
-    On = np.zeros((3, numDeg+1), np.single)
+    On = np.zeros((numDeg+1, 3), np.single)
 
-    for col in range(numDeg - 2):
-        for line in range(3):
-            On[line][col+1] = MT_n[col][line][3]
+    for col in range(numDeg - 1):
+        On[col+1] = MT_n[col, 0:3, 3]
 
-    for line in range(3):
-        On[line][numDeg] = MT[line][3]
+    On[numDeg] = MT[0:3, 3]
 
     return np.around(On, decimals=3)
 
@@ -102,21 +100,52 @@ def degToRad(mat):
     return mat
 
 """
+Compute the Jacobian matrix for linear velocity
+"""
+def linearJacobian(numDeg, Zn, On):
+    Jv = np.zeros([numDeg, 3], np.single)
+
+    for vec in range(numDeg):
+        Jv[vec] = np.cross(Zn[vec], On[numDeg] - On[vec])
+
+    return Jv
+
+"""
+Compute the Jacobian matrix for angular velocity
+"""
+def angularJacobian(degType, numDeg, Zn):
+    Jw = np.zeros([numDeg, 3], np.single)
+
+    for vec in range(numDeg):
+        Jw[vec] = degType[vec]*Zn[vec]
+
+    return Jw
+
+"""
 Compute the linear velocity of the robot
 """
-def linearVelocity(matrices, MT):
-    pass
+def linearVelocity(Jv, q_dot):
+    v = np.matmul(Jv.transpose(), q_dot)
+    return np.around(v, decimals=3)
 
 """
 Compute the angular velocity of the robot
 """
-def angularVelocity(matrices, MT, degType):
-    pass
+def angularVelocity(Jw, q_dot):
+    w = np.matmul(Jw.transpose(), q_dot)
+    return np.around(w, decimals=3)
+
+"""
+Print results of linear and angular velocity
+"""
+def printResults(v, w):
+    print("Linear velocity:\n{}\n".format(v))
+    print("Angular velocity:\n{}\n".format(w))
 
 """
 Main function
 """
-def Analysis(filename="AnalisisVelocidad/data.json"):
+def Analysis(filename="AnalisisVelocidad/seiko.json"):
     data = readDataFromFile(filename)
 
     dh_params = data["dh_params"]
@@ -131,5 +160,13 @@ def Analysis(filename="AnalisisVelocidad/data.json"):
     Zn = getZn(numberDegree, MT_n)
     On = getOn(numberDegree, MT, MT_n)
 
+    Jv = linearJacobian(numberDegree, Zn, On)
+    Jw = angularJacobian(degType, numberDegree, Zn)
+
+    v = linearVelocity(Jv, q_dot)
+    w = angularVelocity(Jw, q_dot)
+
+    printResults(v, w)
+
 if __name__ == "__main__":
-    Analysis()
+    fire.Fire(Analysis)
